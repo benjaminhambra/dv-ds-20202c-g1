a91e35e4c6661d9437c1ca3db2856aeabbb5a687
Benjamin Hambra <benjaminhambra@gmail.com>
2020-11-21 11:18:36 -0300

    Commit inicial del proyecto final

---
+++ src/main/java/ar/edu/davinci/dvds20202cg1/DvDs20202cG1Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20202cg1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20202cG1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20202cG1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/davinci/dvds20202cg1/DvDs20202cG1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20202cg1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20202cG1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
