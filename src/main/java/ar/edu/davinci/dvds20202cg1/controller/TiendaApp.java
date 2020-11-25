fa12a076bba2a009b63150dcc0b547c8c3cdb7a4
Benjamin Hambra <benjaminhambra@gmail.com>
2020-11-25 12:18:33 -0300

    agrego controller y clases tiendaapp y tiendaapprest

---
+++ src/main/java/ar/edu/davinci/dvds20202cg1/controller/TiendaApp.java
@@ -0,0 +1,12 @@
+package ar.edu.davinci.dvds20202cg1.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/tienda/api")
+public abstract class TiendaApp {
+
+	//http://localhost:8091/tienda/api
+}
---
+++ src/main/java/ar/edu/davinci/dvds20202cg1/controller/TiendaAppRest.java
@@ -0,0 +1,11 @@
+package ar.edu.davinci.dvds20202cg1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path="/tienda")
+public abstract class TiendaAppRest {
+
+	//http://localhost:8091/tienda
+}
